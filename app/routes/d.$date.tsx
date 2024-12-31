b1443f69218f7898a7f064f6446db0dc9a46cf8c
Budi Rostiarso <budi@rostiarso.com>
2024-12-31 20:12:45 +0100

    mod (#8)
    * mod
    
    * mod

--- app/routes/d.$date.tsx
+++ app/routes/d.$date.tsx
@@ -8,10 +8,10 @@ import { DATA_NOT_FOUND, JADWAL_SHOLAT } from "~/model/jadwal.server";
 export async function loader({ params }: LoaderArgs) {
 
     const date = params.date;
-    const yearMonth = date!.substring(0,7);
+    const currentMonth = date!.substring(5,7);
     const day = parseInt(date!.substring(8));
     const a =
-      JADWAL_SHOLAT.find((month) => month.bulan === yearMonth)?.jadwal.find(
+      JADWAL_SHOLAT.find((month) => month.bulan === currentMonth)?.jadwal.find(
         (d) => d.Day === day
       ) ?? DATA_NOT_FOUND;
   
